e96e49662f6271e8558c33e59c104f75bfd80d1b
diluka <dt.gpt85@gmail.com>
2026-03-09 19:51:30 +0530

    Add CI pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, DevOps world v1"
+    return "Hello, DevOps World! V1"
 
 if __name__=='main':
     app.run(host='0.0.0.0',port=5000)
